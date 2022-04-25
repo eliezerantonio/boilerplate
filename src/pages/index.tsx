b9e7decf4c6a289d67fccbf829ef97512143817f
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-04-25 03:13:50 +0100

    add husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const foo = 123
 export default function Home() {
   return (
     <div className={styles.container}>
